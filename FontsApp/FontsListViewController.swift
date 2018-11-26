f900398956ee4fd1d8e781f2024f8b80bfdd5126
OguzBayral <oguzbayral93@gmail.com>
2018-11-26 18:53:02 -0500

    Added Fonts List.

--- FontsApp/FontsListViewController.swift
+++ FontsApp/FontsListViewController.swift
@@ -1,103 +1,75 @@
 //
-//  RootViewController.swift
+//  FontsListViewController.swift
 //  FontsApp
 //
-//  Created by Oguz Bayral on 17.11.2018.
+//  Created by Oguz Bayral on 26.11.2018.
 //  Copyright © 2018 CentennialCollege. All rights reserved.
 //
 
 import UIKit
 
-class RootViewController: UITableViewController {
-    
-    private var familyNames: [String]!
-    private var cellPointSize: CGFloat!
-    private var favoritesList: FavoritesList!
-    private static let familyCell = "FamilyName"
-    private static let favoritesCell = "Favorites"
-    
+class FontsListViewController: UITableViewController {
 
+    
+    var fontNames :[String] = []
+    var showsFavorites:Bool = false
+    private var cellPointSize:CGFloat!
+    private static let cellIdentifier = "FontName"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        familyNames = (UIFont.familyNames as [String]).sorted()
         let preferredTableViewFont = UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
         cellPointSize = preferredTableViewFont.pointSize
-        favoritesList = FavoritesList.sharedFavoritesList
         tableView.estimatedRowHeight = cellPointSize
-        
-        
-        
-        
-        
-        
+
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    
+    func fontForDisplay(atIndexPath indexPath: NSIndexPath) -> UIFont {
+        let fontName = fontNames[indexPath.row]
+        return UIFont(name: fontName, size: cellPointSize)!
+    }
 
     
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        tableView.reloadData()
-    }
-    
-    func fontForDisplay(atIndexPath indexPath:NSIndexPath) -> UIFont?{
-        if indexPath.section==0{
-            let familyName = familyNames[indexPath.row]
-            let fontName = UIFont.fontNames(forFamilyName: familyName).first
-            return fontName != nil ? UIFont(name:fontName!, size:cellPointSize) : nil}
-        else{
-            return nil
+       super.viewWillAppear(animated)
+        if showsFavorites {
+            fontNames = FavoritesList.sharedFavoritesList.favorites
+            tableView.reloadData()
         }
     }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
+    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return favoritesList.favorites.isEmpty ? 1 : 2
+        // #warning Incomplete implementation, return the number of sections
+        return 0
     }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return section == 0 ? "All Font Families" : "My Favorite Fonts"
-    }
-    
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return section == 0 ? familyNames.count : 1
+        return fontNames.count
     }
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: RootViewController.familyCell, for: indexPath)
-            cell.textLabel?.font = fontForDisplay(atIndexPath: indexPath as NSIndexPath)
-            cell.textLabel?.text = familyNames[indexPath.row]
-            cell.detailTextLabel?.text = familyNames[indexPath.row]
-            cell.detailTextLabel?.text = familyNames[indexPath.row]
-            return cell
-        }else{
-            return tableView.dequeueReusableCell(withIdentifier: RootViewController.favoritesCell, for: indexPath)
-        }
-    }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let indexPath = tableView.indexPath(for: sender as! UITableViewCell)!
-        let listVC = segue.destination as! FontsListViewController
+        let cell = tableView.dequeueReusableCell(withIdentifier: FontsListViewController.cellIdentifier, for: indexPath)
+        cell.textLabel?.font = fontForDisplay(atIndexPath: indexPath as NSIndexPath)
+        cell.textLabel?.text = fontNames[indexPath.row]
+        cell.detailTextLabel?.text = fontNames[indexPath.row]
         
-        if indexPath.section == 0{
-            let familyName = familyNames[indexPath.row]
-            listVC.fontNames = (UIFont.fontNames(forFamilyName: familyName) as [String]).sorted()
-            listVC.navigationItem.title = familyName
-            listVC.showsFavorites = false
-        }else{
-            listVC.fontNames = favoritesList.favorites
-            listVC.navigationItem.title = "Favorites"
-            listVC.showsFavorites = true
-        }
+        return cell
     }
     
-    
-    
+
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
